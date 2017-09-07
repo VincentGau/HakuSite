20b17a90e962755fd8c4fd96790716dcc94c29ca
Haku <gmkohaku@gmail.com>
2017-09-07 22:50:41 +0800

    add controllers

---
+++ HakuSite/Controllers/HakuController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HakuSite.Controllers
+{
+    public class HakuController : Controller
+    {
+        // GET: Haku
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(string name, int times = 1)
+        {
+            ViewBag.Message = "Hello, " + name;
+            ViewBag.times = times;
+
+            return View();
+        }
+    }
+}
---
+++ HakuSite/Models/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace HakuSite.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public string phone { get; set; }
+        public string mobile { get; set; }
+        public string org { get; set; }
+
+
+    }
+
+    public class HakuDBContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; } 
+    }
+
+
+}
